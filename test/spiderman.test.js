eda0e248d92bf71b07aefe05810106295e2e9c2a
AngelicaRoy <ariadna.espiroy@gmail.com>
2022-04-24 19:15:53 -0500

    Update spiderman.test.js

--- test/spiderman.test.js
+++ test/spiderman.test.js
@@ -12,7 +12,7 @@ describe("Unit Test for Spiderman class", () => {
     });
     
     test('2) Use the method getInfo()',()=>{
-        const tomHolland=new Spiderman("Spiderman Marvel",24,"Tom Holland",4,"Marvel");
+        const tomHolland=new Spiderman("Spiderman Marvel",25,"Tom Holland",5,"Marvel");
         expect(tomHolland.getInfo()).toBe("Hey, I'm Tom Holland from Marvel studio");
     });
   })
